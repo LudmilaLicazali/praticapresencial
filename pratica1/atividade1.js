6af80256a6796db0f9f9fcf645d85c955d131e04
Ludmila <ludmilalicazali@gmail.com>
2024-02-03 14:37:46 -0300

    Correção de erros de execução e sintaxe. E Inclusão do mosulo do pacote prompt

--- pratica1/atividade1.js
+++ pratica1/atividade1.js
@@ -12,21 +12,24 @@ console.log(" 1 -> + : soma " +
 
 valor1 = parseInt(prompt("Digite 1º valor: "))           
 
-
- operacao = parseInt(prompt("Escolha a operação (+ - / * ): ")); 
-
- valor2 = parseInt(prompt("Digite 2º valor: "))
-
-    switch (operacao){
-
-        case 1:    
-            console.log(valor1 + valor2)
-        break;
-
+operacao = parseInt(prompt("Escolha a operação (+ - / * ): ")); 
     
+    if (!isNaN(operacao)){
+    valor2 = parseInt(prompt("Digite 2º valor: "))
+    } else {
+            console.log("Você precisa escolher: 1 para soma (+) " +  
+                                            " 2 para subtração (-) " + 
+                                            " 3 para divisão (/)" + 
+                                            " ou 4 para multiplicação (*)");
+            } 
 
-            }
+operacao = parseInt(prompt("Escolha a operação (+ - / * ): ")); 
 
-
-
-
+valor2 = parseInt(prompt("Digite 2º valor: "))    
+    
+switch (operacao){
+             
+    case 1:    
+            console.log(valor1 + valor2)
+    break;
+    }
