2ef7002718a3eab4fc798690076a868f2da1565d
Ludmila <ludmilalicazali@gmail.com>
2024-02-03 20:25:25 -0300

    Inclusão de comentários para fins de documentação

--- pratica1/atividade1.js
+++ pratica1/atividade1.js
@@ -1,6 +1,9 @@
-// ATIVIDADE PRATICA DO DIA 01/02/2024
+// ATIVIDADE PRATICA DO DIA 01/02/2024 ----> Dupla Ludmila e Mariangele
 
 let prompt = require ('prompt-sync')()
+
+
+// Apresentando quais opções estão ddiponiveis para ecolha
 
 console.log("Serviços disponiveis:");
     
@@ -9,11 +12,15 @@ console.log(" 1 -> + : soma " +
             " 3-> / : divisão " + 
             " 4 -> * : multiplicação "
             );
+// Solicita ao usuario que informe o primeiro numero/valor que fara parte da operação
 
-valor1 = parseInt(prompt("Digite 1º valor: "))           
+valor1 = parseInt(prompt("Digite 1º valor: "))  
+
+//Então solicita a escolha de qual operação deseja que seja realisada
 
 operacao = parseInt(prompt("Escolha a operação (+ - / * ): ")); 
-    
+    // Teste para evitar que seja adicionado um simbolo ao invés do numero que representa a operação.  Se for correto segue solicitando o segundo valor da operação. 
+
     if (!isNaN(operacao)){
     valor2 = parseInt(prompt("Digite 2º valor: "))
     } else {
@@ -22,11 +29,12 @@ operacao = parseInt(prompt("Escolha a operação (+ - / * ): "));
                                             " 3 para divisão (/)" + 
                                             " ou 4 para multiplicação (*)");
             } 
-
+//Se não solicita que escolha novamente a operação desejada. e se for correto ai então solicita o segundo valor
 operacao = parseInt(prompt("Escolha a operação (+ - / * ): ")); 
 
-valor2 = parseInt(prompt("Digite 2º valor: "))    
-    
+valor2 = parseInt(prompt("Digite 2º valor: "))   
+
+    // Apresentação do resultado da operação 
 switch (operacao){
              
     case 1:    
